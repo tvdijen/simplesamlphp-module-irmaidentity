fdaf7ec438a3b79cc916374c2b02adf02e080151
Giacomo <giacomo.tom.petrucci@tiscali.it>
2023-05-14 17:49:47 +0200

    First version, base for testing

--- src/Auth/Source/irmaIdentityProvider.php
+++ src/Auth/Source/irmaIdentityProvider.php
@@ -1,1 +1,32 @@
 <?php
+namespace SimpleSAML\Module\IRMAidp\Auth\Source;
+include "vendor/autoload.php";
+
+//Class irmaIdentityProvider to authenticate the user using IRMA
+class irmaIdentityProvider extends \SimpleSAML\Auth\Source {
+    public \IRMA\Requestor $requestor;
+
+    //class constructor
+    public function __construct($info, $config) {
+        parent::__construct($info, $config);
+        $this->requestor = new \IRMA\Requestor("PEP", "PEP", "testkey.pem"); //TODO: load form config file
+    }
+
+    public function authenticate(){
+        try {
+            $jwt = $this->requestor->getVerificationJwt([
+                [
+                    "label" => "PEP study participant id",
+                    "attributes" => ["irma-demo.PEP.id"]
+                ]
+            ]);
+            var_dump($jwt);
+
+        } catch (\Exception $e) {
+            throw new \SimpleSAML\Error\Exception("Could not get IRMA JWT: " . $e->getMessage());
+        }
+    }
+
+}
+?>
+
